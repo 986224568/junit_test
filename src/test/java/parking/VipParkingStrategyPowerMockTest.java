82d6a2a4f7ebe9f6d1472c95f6726276a24ef166
Domingo <986224568@qq.com>
2020-08-26 20:59:44 +0800

    [junit_test][Sam, Domingo] feature : testCalculateHourlyPrice_givenSunday_thenPriceIsDoubleOfSundayPrice

--- src/test/java/parking/VipParkingStrategyPowerMockTest.java
+++ src/test/java/parking/VipParkingStrategyPowerMockTest.java
@@ -1,10 +1,20 @@
 package parking;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Calendar;
 
+import static org.junit.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(value = {ParkingLot.class})
 public class VipParkingStrategyPowerMockTest {
+
 
     @Test
     public void testCalculateHourlyPrice_givenSunday_thenPriceIsDoubleOfSundayPrice(){
@@ -12,6 +22,13 @@ public class VipParkingStrategyPowerMockTest {
         /* Exercise 6: Write test case for VipParkingStrategy calculateHourlyPrice
         * by using PowerMock to mock static method */
 
+        //given
+        mockStatic(ParkingLot.class);
+        VipParkingStrategy vipParkingStrategy = spy(new VipParkingStrategy());
+        //when
+        when(ParkingLot.getBasicHourlyPrice()).thenReturn(25);
+        //then
+        assertEquals(new Integer(50),vipParkingStrategy.calculateHourlyPrice());
     }
 
     @Test
@@ -19,7 +36,13 @@ public class VipParkingStrategyPowerMockTest {
 
         /* Exercise 6: Write test case for VipParkingStrategy calculateHourlyPrice
          * by using PowerMock to mock static method */
-
+        //given
+        mockStatic(ParkingLot.class);
+        VipParkingStrategy vipParkingStrategy = spy(new VipParkingStrategy());
+        //when
+        when(ParkingLot.getBasicHourlyPrice()).thenReturn(20);
+        //then
+        assertEquals(new Integer(40),vipParkingStrategy.calculateHourlyPrice());
 
     }
 }
